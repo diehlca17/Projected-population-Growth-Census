2f26fd0363a055beaac678fb7fe38796c182ce6d
Castle Diehl <castlediehl@gmail.com>
2022-06-08 17:32:16 -0400

    Population Growth Projection Project, new repo

---
+++ diehlCensusProjectedPopulationGrowth/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace diehlCensusProjectedPopulationGrowth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblEntry = new System.Windows.Forms.Label();
+            this.lblAfterFiveYears = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearDataToolStripMenuItem});
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.clearToolStripMenuItem.Text = "Clear";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitProgramToolStripMenuItem});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // clearDataToolStripMenuItem
+            // 
+            this.clearDataToolStripMenuItem.Name = "clearDataToolStripMenuItem";
+            this.clearDataToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearDataToolStripMenuItem.Text = "Clear Data";
+            this.clearDataToolStripMenuItem.Click += new System.EventHandler(this.clearDataToolStripMenuItem_Click);
+            // 
+            // exitProgramToolStripMenuItem
+            // 
+            this.exitProgramToolStripMenuItem.Name = "exitProgramToolStripMenuItem";
+            this.exitProgramToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitProgramToolStripMenuItem.Text = "Exit Program";
+            this.exitProgramToolStripMenuItem.Click += new System.EventHandler(this.exitProgramToolStripMenuItem_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(300, 79);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(203, 13);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "POPULATION GROWTH CALCULATOR";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(322, 127);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(154, 13);
+            this.lblDescription.TabIndex = 2;
+            this.lblDescription.Text = "10% Percent Increase per Year";
+            // 
+            // lblEntry
+            // 
+            this.lblEntry.AutoSize = true;
+            this.lblEntry.Location = new System.Drawing.Point(278, 205);
+            this.lblEntry.Name = "lblEntry";
+            this.lblEntry.Size = new System.Drawing.Size(110, 13);
+            this.lblEntry.TabIndex = 3;
+            this.lblEntry.Text = "Beginning Population:";
+            // 
+            // lblAfterFiveYears
+            // 
+            this.lblAfterFiveYears.AutoSize = true;
+            this.lblAfterFiveYears.Location = new System.Drawing.Point(322, 262);
+            this.lblAfterFiveYears.Name = "lblAfterFiveYears";
+            this.lblAfterFiveYears.Size = new System.Drawing.Size(174, 13);
+            this.lblAfterFiveYears.TabIndex = 4;
+            this.lblAfterFiveYears.Text = "Population after 5 years of Increase";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(416, 202);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 6;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(347, 351);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(112, 58);
+            this.btnCalc.TabIndex = 7;
+            this.btnCalc.Text = "Calculate Population Growth";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(281, 307);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(285, 20);
+            this.txtOutput.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblAfterFiveYears);
+            this.Controls.Add(this.lblEntry);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Population";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitProgramToolStripMenuItem;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblEntry;
+        private System.Windows.Forms.Label lblAfterFiveYears;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ diehlCensusProjectedPopulationGrowth/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+// Programmer: Castle Diehl
+// Updated: March 23, 2021
+// Purpose: This program calculates the numerical population after five years at an 
+//          increasing percentage variable of 10% per year, using a while loop.  
+
+namespace diehlCensusProjectedPopulationGrowth
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double numYears = 5;                                    // This variable represents the number of years population is increasing.
+            double percentIncrease = .10;                           // This variable represents the percentage the population increases per year.
+            double population = Convert.ToDouble(txtInput.Text);    // This variable represents the population number. 
+            int counter = 1;                                        // This variable serves as a counter in our while loop representing the year accounted for.  
+
+
+            // This while loop calculates the population increase per year and accumulates until it hits the number
+            // of years stored in variable numYears.  
+
+            while (counter <= numYears)
+            {
+                population = (population * percentIncrease) + population;
+                counter = counter + 1;
+            }
+
+            txtOutput.Text = (population).ToString("N1"); // This txtbox provides the result of the calculation.  
+            
+        }
+
+        // Menustrip functionality "Clear"
+        private void clearDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // These lines of code clear the data entered by the user and the data produced in the output.  
+
+            txtInput.Text = " ";
+            txtOutput.Text = " ";
+        }
+
+        // Menustrip functionality "Exit"
+        private void exitProgramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // This line of code exits the program when executed.  
+
+            this.Close();
+        }
+    }
+}
